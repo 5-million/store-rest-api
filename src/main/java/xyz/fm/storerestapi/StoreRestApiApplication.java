a94f32f0794f0f76e14842c64f27359b365cb794
jongseong-mac <kjs3870@naver.com>
2022-03-28 16:23:29 +0900

    init

---
+++ src/main/java/xyz/fm/storerestapi/StoreRestApiApplication.java
@@ -0,0 +1,13 @@
+package xyz.fm.storerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/fm/storerestapi/StoreRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.fm.storerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
