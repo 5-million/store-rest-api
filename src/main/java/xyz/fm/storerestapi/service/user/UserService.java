842622c4055d2d9d1eca5fee183cab9c39a83b3d
jongseong-mac <kjs3870@naver.com>
2022-03-30 16:24:52 +0900

    exists 반환타입 수정

--- src/main/java/xyz/fm/storerestapi/service/user/UserService.java
+++ src/main/java/xyz/fm/storerestapi/service/user/UserService.java
@@ -7,8 +7,8 @@ import xyz.fm.storerestapi.entity.user.BaseUserEntity;
 
 public interface UserService <T extends BaseUserEntity> {
 
-    boolean isExistEmail(String email);
-    boolean isExistPhoneNumber(String phoneNumber);
+    Boolean isExistEmail(String email);
+    Boolean isExistPhoneNumber(String phoneNumber);
     T join(UserJoinRequest request);
     T login(LoginRequest request);
     T modify(UserJoinRequest request);
