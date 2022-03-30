f0d36079e9248210ce3f946ea6a91cffc49f3d1b
jongseong-mac <kjs3870@naver.com>
2022-03-30 23:20:57 +0900

    email의 validation 수정

--- src/main/java/xyz/fm/storerestapi/dto/user/LoginRequest.java
+++ src/main/java/xyz/fm/storerestapi/dto/user/LoginRequest.java
@@ -3,9 +3,13 @@ package xyz.fm.storerestapi.dto.user;
 import xyz.fm.storerestapi.error.ErrorDetail;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public class LoginRequest {
 
+    @NotBlank(message = ErrorDetail.NOT_EMAIL_FORMAT)
+    @NotNull(message = "이메일은 " + ErrorDetail.NOT_NULL)
     @Email(message = ErrorDetail.NOT_EMAIL_FORMAT)
     private String email;
     private String password;
