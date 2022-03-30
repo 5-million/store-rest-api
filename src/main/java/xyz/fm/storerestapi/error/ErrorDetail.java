0022a5abfd00e88c5e02dbe3c70565d92c985199
jongseong-mac <kjs3870@naver.com>
2022-03-30 23:17:01 +0900

    detail message 추가

--- src/main/java/xyz/fm/storerestapi/error/ErrorDetail.java
+++ src/main/java/xyz/fm/storerestapi/error/ErrorDetail.java
@@ -2,6 +2,7 @@ package xyz.fm.storerestapi.error;
 
 public class ErrorDetail {
 
+    public static final String NOT_BLANK = "1 글자 이상이어야 합니다.";
     public static final String NOT_NULL = "필수 항목입니다.";
     public static final String NOT_EMAIL_FORMAT = "이메일 형식이어야 합니다.";
     public static final String NOT_PHONE_NUMBER_FORMAT = "휴대폰 번호 형식이어야 합니다.(ex. 01x-xxxx-xxxx or 01xxxxxxxxx)";
