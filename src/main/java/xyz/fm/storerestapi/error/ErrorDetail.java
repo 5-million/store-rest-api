25b0f5d6fbfd085dc7611212ce9dbb222f878852
jongseong-mac <kjs3870@naver.com>
2022-03-30 18:50:39 +0900

    이름변경 및 디테일 추가

---
+++ src/main/java/xyz/fm/storerestapi/error/ErrorDetail.java
@@ -0,0 +1,7 @@
+package xyz.fm.storerestapi.error;
+
+public class ErrorDetail {
+
+    public static final String NOT_EMAIL_FORMAT = "It must be in email format.";
+    public static final String NOT_PHONE_NUMBER_FORMAT = "Must be in mobile phone number format.";
+}
