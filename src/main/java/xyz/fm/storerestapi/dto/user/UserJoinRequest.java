29eaed2232efc160794921cf43b9993e9bd95b71
jongseong-mac <kjs3870@naver.com>
2022-03-31 23:18:53 +0900

    수정

--- src/main/java/xyz/fm/storerestapi/dto/user/UserJoinRequest.java
+++ src/main/java/xyz/fm/storerestapi/dto/user/UserJoinRequest.java
@@ -1,4 +1,10 @@
 package xyz.fm.storerestapi.dto.user;
 
 public interface UserJoinRequest {
+
+    String getEmail();
+    String getName();
+    String getPassword();
+    String getConfirmPassword();
+    String getPhoneNumber();
 }
