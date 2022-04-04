0f02028f88860a6ec482fc466cf66d020b35b429
jongseong-mac <kjs3870@naver.com>
2022-04-04 23:03:23 +0900

    JwtTokenUtil 추가

---
+++ src/main/java/xyz/fm/storerestapi/jwt/JwtTokenUtil.java
@@ -0,0 +1,34 @@
+package xyz.fm.storerestapi.jwt;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import xyz.fm.storerestapi.entity.user.BaseUserEntity;
+
+import javax.servlet.http.Cookie;
+import java.util.UUID;
+
+@Component
+public class JwtTokenUtil {
+
+    // spring security, jwt 구현 시 수정
+
+    public static final String JWT_KEY = "STORE_AUTH_TOKEN";
+    public static final String JWT_VALUE_PREFIX = "Bearer ";
+
+    @Value("${jwt.expired}")
+    private Integer expired;
+
+    public Cookie generateTokenCookie(BaseUserEntity user) {
+        Cookie cookie = new Cookie(JWT_KEY, JWT_VALUE_PREFIX + generateToken(user));
+        cookie.setMaxAge(expired);
+        return cookie;
+    }
+
+    public String generateToken(BaseUserEntity user) {
+        return UUID.randomUUID().toString();
+    }
+
+    public Integer getExpired() {
+        return expired;
+    }
+}
