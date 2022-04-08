e890a5db66539d478c526b188f0c83e65aa442ad
jongseong-mac <kjs3870@naver.com>
2022-04-08 17:14:32 +0900

    vendor 관련 entity 추가

---
+++ src/main/java/xyz/fm/storerestapi/entity/user/vendor/Vendor.java
@@ -0,0 +1,86 @@
+package xyz.fm.storerestapi.entity.user.vendor;
+
+import xyz.fm.storerestapi.entity.BaseTimeEntity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "STORE_VENDOR")
+public class Vendor extends BaseTimeEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "vendor_id")
+    private Long id;
+
+    private String vendorName;
+    private String ceo;
+
+    @Column(unique = true)
+    private String registrationNumber;
+
+    private String location;
+
+    @OneToMany(mappedBy = "vendor", cascade = CascadeType.ALL)
+    private List<VendorManager> managers = new ArrayList<>();
+
+    protected Vendor() {/* empty */}
+
+    private Vendor(Builder builder) {
+        this.id = builder.id;
+        this.vendorName = builder.vendorName;
+        this.ceo = builder.ceo;
+        this.registrationNumber = builder.registrationNumber;
+        this.location = builder.location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public String getCeo() {
+        return ceo;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public List<VendorManager> getManagers() {
+        return managers;
+    }
+
+    //== builder ==//
+    public static class Builder {
+        private Long id;
+        private final String vendorName;
+        private final String ceo;
+        private final String registrationNumber;
+        private final String location;
+
+        public Builder(String vendorName, String ceo, String registrationNumber, String location) {
+            this.vendorName = vendorName;
+            this.ceo = ceo;
+            this.registrationNumber = registrationNumber;
+            this.location = location;
+        }
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Vendor build() {
+            return new Vendor(this);
+        }
+    }
+}
---
+++ src/main/java/xyz/fm/storerestapi/entity/user/vendor/VendorManager.java
@@ -0,0 +1,91 @@
+package xyz.fm.storerestapi.entity.user.vendor;
+
+import xyz.fm.storerestapi.entity.user.BaseUserEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "STORE_VENDOR_MANAGER")
+public class VendorManager extends BaseUserEntity {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vendor_id")
+    private Vendor vendor;
+
+    @Enumerated(value = EnumType.STRING)
+    private VendorRole role;
+
+    private Boolean permission;
+
+    protected VendorManager() {/* empty */}
+
+    private VendorManager(Builder builder) {
+        super(builder.email, builder.name, builder.password, builder.phoneNumber);
+        this.id = builder.id;
+        this.vendor = builder.vendor;
+        this.role = builder.role;
+        this.permission = builder.permission;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Vendor getVendor() {
+        return vendor;
+    }
+
+    public VendorRole getRole() {
+        return role;
+    }
+
+    public Boolean getPermission() {
+        return permission;
+    }
+
+    //== builder ==//
+    public static class Builder {
+        private Long id;
+        private final String email;
+        private final String name;
+        private final String password;
+        private final String phoneNumber;
+        private Vendor vendor;
+        private VendorRole role = VendorRole.VENDOR_MANAGER;
+        private Boolean permission = false;
+
+        public Builder(String email, String name, String password, String phoneNumber) {
+            this.email = email;
+            this.name = name;
+            this.password = password;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder vendor(Vendor vendor) {
+            this.vendor = vendor;
+            return this;
+        }
+
+        public Builder role(VendorRole role) {
+            this.role = role;
+            return this;
+        }
+
+        public Builder permission(Boolean permission) {
+            this.permission = permission;
+            return this;
+        }
+
+        public VendorManager build() {
+            return new VendorManager(this);
+        }
+    }
+}
