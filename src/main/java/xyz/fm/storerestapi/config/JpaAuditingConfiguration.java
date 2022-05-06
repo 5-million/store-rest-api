47910b29921d4bff6caf5ae5d68ebb4916645964
jongseong-mac <kjs3870@naver.com>
2022-05-06 15:47:05 +0900

    JpaAuditing 설정 추가

---
+++ src/main/java/xyz/fm/storerestapi/config/JpaAuditingConfiguration.java
@@ -0,0 +1,7 @@
+package xyz.fm.storerestapi.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
---
+++ src/main/java/xyz/fm/storerestapi/entity/BaseTimeEntity.java
@@ -0,0 +1,30 @@
+package xyz.fm.storerestapi.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate = LocalDateTime.now();
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate = LocalDateTime.now();
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+}
